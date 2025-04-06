db8c4340f747c7a00709c338a95b11343f5f61ed
kali <kali@kali.tamil>
2025-04-06 21:40:25 +0530

    Update homepage message

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+# app.py
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello from Docker this is an updated code to check the CI/CD pipeline!"
+def home():
+    return "🎉 Updated Version Deployed Successfully!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
