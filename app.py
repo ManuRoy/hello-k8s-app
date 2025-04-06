0762e29b152cd0883b2195686dcedb8eb6914bdb
kali <kali@kali.tamil>
2025-04-06 22:14:28 +0530

    Updateddd  homepage message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🎉 Updated Version Deployed Successfully!"
+    return "🎉 Updateddddddddddddddd  Version Deployed Successfully!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
